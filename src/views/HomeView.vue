1bc5f2bb3b5f713fe83b9bff8e2862b60179b4fd
repoleved08 <geekbii08@gmail.com>
2024-04-11 17:08:11 +0300

    Baas Techx

---
+++ src/views/HomeView.vue
@@ -0,0 +1,10 @@
+<script setup>
+
+</script>
+
+<template>
+  <main>
+    <h1 class="text-4xl font-bold text-center">Welcome to the Home Page</h1>
+    <p class="text-center mt-4">This is the home page of the app</p>
+  </main>
+</template>
