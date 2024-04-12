26462feef7a379baf3595331abb592133ca7a6b3
repoleved08 <geekbii08@gmail.com>
2024-04-12 11:33:04 +0300

    added auth login

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,20 @@
 <template>
-  <Header/>
-  <Hero/>
-  <Logos/>
-  <Features/>
-  <Pricing/>
-  <Accordion/>
-  <Footer/>
+  <Header />
+  <Hero />
+  <Logos />
+  <Features />
+  <Pricing />
+  <Accordion />
+  <Footer />
+  <RouterView />
 </template>
 
 <script setup>
-import Header from './components/Header.vue'
-import Hero from './components/Hero.vue'
-import Logos from './components/Logos.vue'
-import Features from './components/Features.vue'
-import Pricing from './components/Pricing.vue'
-import Accordion from './components/Accordion.vue'
-import Footer from './components/Footer.vue'
+import Header from '@/components/Header.vue'
+import Hero from '@/components/Hero.vue'
+import Logos from '@/components/Logos.vue'
+import Features from '@/components/Features.vue'
+import Pricing from '@/components/Pricing.vue'
+import Accordion from '@/components/Accordion.vue'
+import Footer from '@/components/Footer.vue'
 </script>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -3,8 +3,5 @@
 </script>
 
 <template>
-  <main>
-    <h1 class="text-4xl font-bold text-center">Welcome to the Home Page</h1>
-    <p class="text-center mt-4">This is the home page of the app</p>
-  </main>
+
 </template>
